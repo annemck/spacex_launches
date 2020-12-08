5f063516fd72795187979fe8ba60b6328d18679d
annemck <annekh88@gmail.com>
2020-12-08 17:02:32 +0000

    images added to import for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import logo from './assets/images/spacex-logo.png';
+import launchImage from './assets/images/launch-home.png';
 import LaunchContainer from './components/launches/launchContainer';
 
 const App = () => {
   return (
     <div className="App">
-      <p>running</p>
       <LaunchContainer/>
     </div>
   );
