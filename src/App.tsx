24d3007af74f0276785f3788b3c0eb3eccf57905
annemck <annekh88@gmail.com>
2020-12-08 16:42:48 +0000

    launch list item filters down to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import LaunchContainer from './components/launches/launchContainer';
+
 const App = () => {
   return (
     <div className="App">
       <p>running</p>
+      <LaunchContainer/>
     </div>
   );
 }
