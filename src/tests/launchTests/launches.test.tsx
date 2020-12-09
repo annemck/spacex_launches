75a0e114cfcc431d9b9ab3c4c7aa8293d8d66beb
annemck <annekh88@gmail.com>
2020-12-09 16:35:48 +0000

    tests are passing

--- src/tests/launchTests/launches.test.tsx
+++ src/tests/launchTests/launches.test.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import { render, fireEvent, waitForElement } from "@testing-library/react";
 import LaunchContainer from '../../components/launches/launchContainer';
 import LaunchListItem from '../../components/launches/launchListItem';
+import {fetchLaunchDetails} from '../../API';
+import App from '../../App';
+
+let launchList;
 
 
-it('all 111 API items are passed to LaunchListItem component', () => {
-  const {container} = render(<LaunchContainer/>);
-  expect(container.findAll(<LaunchListItem/>).length).toBe(111);
+it('all 111 items are returned from API', () => {
+  (async () => {
+    launchList = await fetchLaunchDetails();
+    expect(launchList.length).toBe(111);
+  })
+})
+
+it('check 1st and last launches from API are showing', () => {
+  (async () => {
+    const {container} = await render(<App/>);
+    expect(container).toHaveTextContent('1 FalconSat');
+    expect(container).toHaveTextContent('110 SXM-7');
+  })
 })
 
 // it('launch list item is showing text', () => {
