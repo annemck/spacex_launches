f21ddd79905c280cc99f92149ed5d1a8c882dc4c
annemck <annekh88@gmail.com>
2020-12-10 10:06:55 +0000

    button container created

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 // import logo from './assets/images/spacex-logo.png';
 // import launchImage from './assets/images/launch-home.png';
 import LaunchContainer from './components/launches/launchContainer';
+import ButtonContainer from './components/buttons/buttonContainer';
 import {fetchLaunchDetails} from './API';
 
 const App = () => {
@@ -21,7 +22,12 @@ const App = () => {
   
   return (
     <div className="App">
+      <div>
+        <ButtonContainer/>
+      </div>
+      <div>
       {loaded ? <LaunchContainer launches={launches}/> : <p>Loading...</p>}
+      </div>
     </div>
   );
 }
---
+++ src/components/buttons/buttonContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ButtonContainer = () => {
+  return(
+    <p>this is the button container</p>
+  )
+}
+
+export default ButtonContainer;
