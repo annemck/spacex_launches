629d3d71a72bf22ba3b3f27e3d5f2fe693509bc7
annemck <annekh88@gmail.com>
2020-12-11 18:40:54 +0000

    basic list styling added

--- src/components/launches/launchListItem.tsx
+++ src/components/launches/launchListItem.tsx
@@ -9,12 +9,14 @@ const LaunchListItem: React.FC<Props> = ({launches}) => {
   return(
     <div>
     {launches.map(launch => (
+      <div className="list_element">
       <li key={launch.mission_name}>
         <p className="flight_number">#{launch.flight_number}</p>
         <p className="mission_name">{launch.mission_name}</p>
         <p className="launch_date">{launch.launch_date}</p>
         <p className="rocket_name">{launch.rocket_name}</p>
       </li>
+      </div>
     ))}
     </div>
   )
