b7d2cbb965b6c6ee9824fe9b929a096d3d972780
annemck <annekh88@gmail.com>
2020-12-14 11:37:10 +0000

    commit before new library install

--- src/tests/launchTests/launches.test.tsx
+++ src/tests/launchTests/launches.test.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { render, fireEvent, waitForElement } from "@testing-library/react";
+import { axe, toHaveNoViolations }  from "jest-axe";
 import LaunchContainer from '../../components/launches/launchContainer';
 import LaunchListItem from '../../components/launches/launchListItem';
 import {fetchLaunchDetails} from '../../API';
 import App from '../../App';
 
 let launchList;
+const fetch = require('node-fetch');
+expect.extend(toHaveNoViolations);
+
+
+it('should demonstrate this matcher`s usage with react testing library', async () => {
+  const { container } = render(<App/>)
+  const results = await axe(container)
+
+  expect(results).toHaveNoViolations()
+})
 
 
 it('all 111 items are returned from API', () => {
